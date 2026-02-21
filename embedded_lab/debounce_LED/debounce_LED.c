0ab379927c27da6a0d8f57d85744f58a5b6759f8
UtkarshS72 <148773189+UtkarshS72@users.noreply.github.com>
2026-02-22 04:49:39 +0530

    Update debounce_LED.c
    properly configured IO-MUX of GPIO5

--- embedded_lab/debounce_LED/debounce_LED.c
+++ embedded_lab/debounce_LED/debounce_LED.c
@@ -4,6 +4,7 @@
 #define GPIO_OUT_REG     0x3FF44004
 #define GPIO_ENABLE_REG  0x3FF44020
 #define GPIO_IN_REG      0x3FF4403C
+#define IO_MUX_GPIO5_REG 0x3FF4906C
 
 static inline uint32_t cpu_cycles() {
   uint32_t ccount;
@@ -12,18 +13,23 @@ static inline uint32_t cpu_cycles() {
 }
 
 void setup() {
-  Serial.begin(115200);
 
   volatile uint32_t *gpio_out = (uint32_t*) GPIO_OUT_REG;
   volatile uint32_t *gpio_en  = (uint32_t*) GPIO_ENABLE_REG;
   volatile uint32_t *gpio_in  = (uint32_t*) GPIO_IN_REG;
+  volatile uint32_t *gpio5_config = (uint32_t*) IO_MUX_GPIO5_REG;
 
-  // LED on GPIO2
-  *gpio_en |= (1u << 2);
 
-  // Button on GPIO5 with pull-up 
-  pinMode(5, INPUT_PULLUP);
-  *gpio_en &= ~(1u << 5); // ensure input
+  /* select GPIO function (MCU_SEL = 0 for Function 0 = GPIO on ESP32 IO_MUX pins) */
+  *gpio5_config &= ~(0x7u << 12);  // clear bits 12..14
+  *gpio5_config |= (1u << 8); // FUN_WPU pin 5 internal pull up enabled
+  *gpio5_config |= (1u << 9); // FUN_IE pin 5 can sense inputs, this needs to be explicit
+  *gpio5_config &= ~(1u << 7); // FUN_WPD pin 5 internal pull down disabled, this also need to be explicit, even if it seems implicit
+  
+  *gpio_en &= ~(1u << 5); // ensures pin 5 will not drive output
+
+  
+  *gpio_en |= (1u << 2); // ensure pin 2 (led) is an output
 
   int last_raw = ((*gpio_in) >> 5) & 1; // last value measured at 5th bit
   int stable_state = 1; // it is a pull_up
@@ -44,11 +50,11 @@ void setup() {
       
     }
 
-    if((last_raw == raw) && ((cpu_cycles() - last_change_time) >= window) && (raw != stable_state)){ // here the last_raw == raw is not really needed, I'm leaving it here to prevent future confusion
+    if((last_raw == raw) && ((cpu_cycles() - last_change_time) >= window) && (raw != stable_state)){
       stable_state = raw;
       if((prev_stable_state == 1) && (stable_state == 0)){
         led_state ^= 1;
-        *gpio_out = (*gpio_out&~(1u << 2))|(led_state << 2);
+        *gpio_out = (*gpio_out&~(1u << 2))|((uint32_t)led_state << 2);
       }
       prev_stable_state = stable_state;
     }
